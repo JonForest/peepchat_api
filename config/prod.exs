e6d6dabcd59936755e324cf04b8db9c7ee5854c3
jonathanh <jonforest@gmail.com>
2016-05-30 23:25:56 +1200

    Fixes

--- config/prod.exs
+++ config/prod.exs
@@ -13,6 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   url: [host: "gentle-fortress-37747.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
@@ -68,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
